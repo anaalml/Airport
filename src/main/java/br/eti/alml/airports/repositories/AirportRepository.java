f54d31c68a3c24c7314a708a5cd89898e6d412ed
Ana Luiza Martins Lima <analuiza_alml@hotmail.com>
2025-11-27 09:45:53 -0300

    Término da Fase 3 – Implementado endpoint FindByCityIgnoreCase. - Update AirportRepository – Adicionado assinatura de método findByCityIgnoreCase; - Update AirportService – Adicionado método findByCity - Update AirportController – Adicionado endpoint /city/{cityName}

--- src/main/java/br/eti/alml/airports/repositories/AirportRepository.java
+++ src/main/java/br/eti/alml/airports/repositories/AirportRepository.java
@@ -1,8 +1,11 @@
 package br.eti.alml.airports.repositories;
 
 import br.eti.alml.airports.entities.Airport;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AirportRepository extends JpaRepository <Airport, Long> {
+    
+    List<Airport> findByCityIgnoreCase(String city);
     
 }
